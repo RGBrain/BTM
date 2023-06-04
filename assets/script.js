bab327fd086d956121a5384f79da5eb8221127f7
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-06-04 12:15:08 +0100

    add form reset

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
-
+// Initialise EmailJS
 (function() {
     emailjs.init('EdfyCsZWNaGrPgZOe');
 })();
 
-
+// Clear form 
+function clearForm() {
+    document.getElementById('contact-form').reset()
+}
 
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
@@ -17,5 +20,6 @@ window.onload = function() {
             }, function(error) {
                 console.log('FAILED...', error);
             });
+        clearForm();
     });
 }
